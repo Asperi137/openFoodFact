98c40badc537a9855b4b53540c058da9d9c2e7bf
Jerome CHAPUT <106680913+Asperi137@users.noreply.github.com>
2022-09-27 15:12:16 +0200

    début de reflection pour le sass

--- js/indexBrut.js
+++ js/indexBrut.js
@@ -3,28 +3,35 @@ boutonRechercher.addEventListener("click", async function (e) {
 	e.preventDefault();
 	//récupération des données pour les afficher
 	const codeBarre = document.getElementById("txt-search").value;
-	requete = "json/3017620424403.json";
+	//requete = "json/3017620424403.json";
 	//requete = "json/3083681081534.json";
-	//const requete = "https://world.openfoodfacts.org/api/v2/product/" + codeBarre;
+	const requete = "https://world.openfoodfacts.org/api/v2/product/" + codeBarre;
 	const produit = await fetch(requete).then((produit) => produit.json());
+
 	//affichage de l'image du produit
 	const imageProduit = document.getElementById("img-produit");
 	imageProduit.src = produit.product.image_url;
+
 	//affichage du nom du produit
 	const nomProduit = document.getElementById("nom-produit");
 	nomProduit.innerText = produit.product.product_name;
+
 	//affichage du fabriquant du produit
 	const nomProducteur = document.getElementById("nom-producteur");
 	nomProducteur.innerText = produit.product.brands;
+
 	//affichage de la quantité présente dans le produit
 	const quantity = document.getElementById("quantity");
 	quantity.innerText = produit.product.quantity;
+
 	//affichage du nutriscore du produit
 	const imageNutriscore = document.getElementById("img-nutriscore");
 	imageNutriscore.src = "images/nutriscore" + produit.product.nutriscore_grade.toUpperCase() + ".jpg";
+
 	//affichage du groupe nova du produit
 	const imageNova = document.getElementById("img-nova");
 	imageNova.src = "images/nova" + produit.product.nova_group + ".jpg";
+
 	//affichage de l'eco-score du produit
 	const imgEcoscore = document.getElementById("img-ecoscore");
 	imgEcoscore.src = "images/ecoscore-" + produit.product.ecoscore_grade + ".svg";
@@ -36,6 +43,7 @@ boutonRechercher.addEventListener("click", async function (e) {
 	} else {
 		ingredients.innerText = produit.product.ingredients_text;
 	}
+
 	//affichage des alergènes du produit
 	const alergenes = document.getElementById("alergene");
 	let listAlergenes = "";
@@ -48,35 +56,55 @@ boutonRechercher.addEventListener("click", async function (e) {
 	} else {
 		alergenes.innerText = "aucun alergènes connus";
 	}
+
 	//affichage des nutriment pour 100g avec le "niveau" sous forme de pastil de couleur
 	const nutriments = produit.product.nutriments;
+	
 	//Calories
 	const energy = document.getElementById("energy");
 	const energie100g = nutriments["energy-kcal_100g"];
 	const ajr = " (" + energie100g / 20 + "% des AJR)";
-	energy.innerHTML = "<td>Calories : </td><td>" + energie100g + nutriments["energy-kcal_unit"] + ajr + "</td>";
+	energy.innerHTML =
+		"<td>Calories : </td><td>" + energie100g + nutriments["energy-kcal_unit"] + "</td><td>" + ajr + "</td>";
+	
 	//sucre
 	const sucre = document.getElementById("sugars");
-	sucre.innerHTML =
-		"<td>Glucides : </td><td>" +
-		nutriments.sugars_100g +
-		nutriments.sugars_unit +
-		nutrimentlvl(produit, "sugars") +
+		sucre.innerHTML =
+			"<td>Glucides : </td><td>" +
+			nutriments.sugars_100g +
+			nutriments.sugars_unit +
+			"</td><td>" +
+			nutrimentlvl(produit, "sugars") +
 		"</td>";
+	
 	//Sodium
 	const salt = document.getElementById("salt");
 	salt.innerHTML =
-		"<td>Sodium : </td><td>" + nutriments.salt_100g + nutriments.salt_unit + nutrimentlvl(produit, "salt") + "</td>";
+		"<td>Sodium : </td><td>" +
+		nutriments.salt_100g +
+		nutriments.salt_unit +
+		"</td><td>" +
+		nutrimentlvl(produit, "salt") +
+		"</td>";
+	
 	//Lipides
 	const fat = document.getElementById("fat");
 	fat.innerHTML =
-		"<td>Lipides :  </td><td>" + nutriments.fat_100g + nutriments.fat_unit + nutrimentlvl(produit, "fat") + "</td>";
+		"<td>Lipides :  </td><td>" +
+		nutriments.fat_100g +
+		nutriments.fat_unit +
+		"</td><td>" +
+		nutrimentlvl(produit, "fat") +
+		"</td>";
+	
 	//graisse saturés
 	const saturated = document.getElementById("saturated-fat");
+
 	saturated.innerHTML =
-		"<td>graisse saturés : </td><td>" +
+		"<td>Acide gras saturés : </td><td>" +
 		nutriments["saturated-fat_100g"] +
 		nutriments["saturated-fat_unit"] +
+		"</td><td>" +
 		nutrimentlvl(produit, "saturated-fat") +
 		"</td>";
 });
@@ -94,3 +122,4 @@ function nutrimentlvl(produit,nutriment) {
 		}
 	return level;
 }
+
